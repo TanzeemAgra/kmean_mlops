3770eba1c80031068a9f8b9636bcb668062664f7
Xerxez <info@xerxez.in>
2023-10-04 19:07:39 +0530

    CookieCutter Template Deployment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is to demostrate mlops orchestration for kmean clustering for unsupervised learning',
+    author='Xerxez Solutions',
+    license='MIT',
+)
